ab10272ea495b671e999db56b9585f14a73b8677
Mark <contact.now.markchen@gmail.com>
2024-06-27 03:25:51 +0200

    update built nodes name

--- build/point-cloud-process/CMakeFiles/point-cloud-process_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/point-cloud-process_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/point-cloud-process_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/point-cloud-process/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mark/thesis/src/point-cloud-process
-# Build directory: /home/mark/thesis/build/point-cloud-process
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
