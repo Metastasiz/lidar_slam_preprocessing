df0b77815499be886872ae8e97ca2a26aa7f7942
Mark <contact.now.markchen@gmail.com>
2024-06-27 04:49:48 +0200

    add screenshots

--- src/slam_1_pc-process/src/1_import_pcd.cpp
+++ src/slam_1_pc-process/src/1_import_pcd.cpp
@@ -5,21 +5,24 @@
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 
+typedef pcl::PointXYZ PointT;
+
 int main()
 {
-    pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2());
+    // ********************************		Import PCD
+    pcl::PointCloud<PointT>::Ptr input_cloud (new pcl::PointCloud<PointT>);
 
     pcl::PCDReader cloud_reader;
 
-    std::string import_file_pcd         = "tb3_world.pcd";
+    std::string import_file_pcd         = "raw_world.pcd";
     std::string dir_pcd                 = "slam_1_pc-process/data";
     std::filesystem::path path_ros_ws   = std::filesystem::current_path();
     std::filesystem::path path_dir_pcd  = path_ros_ws/"src"/dir_pcd;
     std::string path_import             = path_dir_pcd/import_file_pcd;
 
-    cloud_reader.read(path_import,*cloud);
+    cloud_reader.read(path_import,*input_cloud);
     
-    std::cout << "Number of points: " << cloud->width * cloud->height << std::endl;
+    std::cout << "Number of points: " << input_cloud->width * input_cloud->height << std::endl;
 
     return 0;
 }
--- src/slam_1_pc-process/src/2_manual_pcd.cpp
+++ src/slam_1_pc-process/src/2_manual_pcd.cpp
@@ -4,28 +4,32 @@
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
 
+typedef pcl::PointXYZ PointT;
+
 int main()
 {
-    pcl::PointCloud<pcl::PointXYZ> cloud ;
+    // ********************************		Import PCD
+    pcl::PointCloud<PointT> export_cloud;
 
     pcl::PCDWriter cloud_writer;
 
-    std::string export_file_pcd         = "manual_pcd.pcd";
+    std::string export_file_pcd_after   = "2_world.pcd";
     std::string dir_pcd                 = "slam_1_pc-process/data";
-	std::filesystem::path path_ros_ws   = std::filesystem::current_path();
+    std::filesystem::path path_ros_ws   = std::filesystem::current_path();
     std::filesystem::path path_dir_pcd  = path_ros_ws/"src"/dir_pcd;
-    std::string path_export             = path_dir_pcd/export_file_pcd;
+    std::string path_export_after       = path_dir_pcd/export_file_pcd_after;
 
-    cloud.push_back(pcl::PointXYZ(1.0 ,2.0 ,3.0));
-    cloud.push_back(pcl::PointXYZ(4.0 ,5.0 ,3.0));
-    cloud.push_back(pcl::PointXYZ(7.0 ,8.0 ,3.0));
-    cloud.push_back(pcl::PointXYZ(10.0,11.0,3.0));
-    cloud.push_back(pcl::PointXYZ(12.0,14.0,3.0));
+    // ********************************		Manually Adding Point Cloud Data
+    export_cloud.push_back(PointT(1.0 ,2.0 ,3.0));
+    export_cloud.push_back(PointT(4.0 ,5.0 ,3.0));
+    export_cloud.push_back(PointT(7.0 ,8.0 ,3.0));
+    export_cloud.push_back(PointT(10.0,11.0,3.0));
+    export_cloud.push_back(PointT(12.0,14.0,3.0));
 
-    cloud_writer.write<pcl::PointXYZ>(path_export,cloud);
+    cloud_writer.write<PointT>(path_export_after,export_cloud);
 
-    std::cout << "Manually created: " << path_export << std::endl;
-    std::cout << "Number of points: " << cloud.size() << std::endl;
+    std::cout << "Manually created: " << path_export_after << std::endl;
+    std::cout << "Number of points: " << export_cloud.size() << std::endl;
 
     return 0;
 
